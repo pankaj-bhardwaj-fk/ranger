354470940ac9e471fdcd4d5b023179dd88997b23
pankaj.bhardwaj <pankaj.bhardwaj@flipkart.com>
2015-08-19 16:01:53 +0530

    version bump up and use of hardthresold

--- src/main/java/com/flipkart/ranger/finder/LeastActiveServiceNodeSelector.java
+++ src/main/java/com/flipkart/ranger/finder/LeastActiveServiceNodeSelector.java
@@ -14,8 +14,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class LeastActiveServiceNodeSelector<T> implements ServiceNodeSelector<T> {
     private ThreadLocal<Map<ServiceNode<T>, Integer>> requestMapper= new ThreadLocal<>();
     private ThreadLocal<ServiceNode<T> > serviceNodeThreadLocal = new ThreadLocal();
+    private static final ThreadLocal<Integer> count =  new ThreadLocal<Integer>() {
+                @Override protected Integer initialValue() {
+                    return 0;
+                }
+            };
+
     private static Integer softThresold = 50;
     private static Integer hardThresold = 70;
+    private static Integer sigmaLoad = 100;
     @Override
     public ServiceNode<T> select(List<ServiceNode<T>> serviceNodes) {
         ServiceNode<T> nodes = null;
@@ -23,10 +30,14 @@ public class LeastActiveServiceNodeSelector<T> implements ServiceNodeSelector<T>
         while(found){
             nodes = serviceNodes.get(ThreadLocalRandom.current().nextInt(serviceNodes.size()));
             if(requestMapper.get().containsKey(nodes)){
-                if(requestMapper.get().get(nodes) < softThresold){
+                if(requestMapper.get().get(nodes) < softThresold || requestMapper.get().get(nodes) < hardThresold && count.get() < sigmaLoad){
                     found = false;
-                    requestMapper.get().put(nodes, requestMapper.get().get(nodes)+1);
+                    Integer value = requestMapper.get().get(nodes)+1;
+                    requestMapper.get().put(nodes, value);
                     serviceNodeThreadLocal.set(nodes);
+                    if(value +1 >= softThresold){
+                        count.set(count.get()+1);
+                    }
                 }
             }
         }
@@ -35,7 +46,15 @@ public class LeastActiveServiceNodeSelector<T> implements ServiceNodeSelector<T>
 
     @Override
     public void ack() {
+        Boolean flag = false;
+        if(requestMapper.get().get(serviceNodeThreadLocal.get()) >= softThresold){
+            flag = true;
+        }
         requestMapper.get().put(serviceNodeThreadLocal.get(), requestMapper.get().get(serviceNodeThreadLocal.get())+1);
+        if(requestMapper.get().get(serviceNodeThreadLocal.get()) < softThresold && flag){
+            count.set(count.get() - 1);
+        }
+
     }
 }
 
