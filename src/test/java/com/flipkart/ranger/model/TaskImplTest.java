1e461f6d45f1a74bead35ab3b9c4deeabf0e4d45
pankaj.bhardwaj <pankaj.bhardwaj@flipkart.com>
2015-08-27 13:02:55 +0530

    impl complete

--- src/test/java/com/flipkart/ranger/model/TaskImplTest.java
+++ src/test/java/com/flipkart/ranger/model/TaskImplTest.java
@@ -1,9 +1,16 @@
 package com.flipkart.ranger.model;
 
+import java.util.List;
+
 /**
  * Created by pankaj.bhardwaj on 19/08/15.
  */
 public class TaskImplTest<T> implements Task<T> {
+    @Override
+    public void initialize(List<Object> objectList) {
+
+    }
+
     @Override
     public <R> R execute(ServiceNode<T> node) {
         return null;
