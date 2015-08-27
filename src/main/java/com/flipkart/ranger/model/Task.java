23aac6e2a33283558859d0c0cd382e2b411f920c
pankaj.bhardwaj <pankaj.bhardwaj@flipkart.com>
2015-08-27 13:01:06 +0530

    task class adding initialize

--- src/main/java/com/flipkart/ranger/model/Task.java
+++ src/main/java/com/flipkart/ranger/model/Task.java
@@ -1,10 +1,14 @@
 package com.flipkart.ranger.model;
 
+import java.util.List;
+
 /**
  * Created by pankaj.bhardwaj on 19/08/15.
  */
 
 public interface Task<T> {
+
+    void initialize(List<Object> objectList);
     <R> R execute(ServiceNode<T> node);
 }
 
